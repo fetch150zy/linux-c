8a95ff9060cb33392570658c5cc6f32b9cfa4599
fetch150zy <Mars_zhewei@outlook.com>
2023-02-03 13:42:38 +0800

    demo of lseek truncate ...

--- io/ext.c
+++ io/ext.c
@@ -1,3 +1,9 @@
+/**
+ * ext.c (fetch150zy)
+ * usage of lseek() truncate() ftruncate()
+ * copyleft (c) 2023
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,45 +12,65 @@
 #include <assert.h>
 #include <string.h>
 
+#include "../utils/utils.h"
+
+
+// #define EXT
+#define EXT_
 
 extern int errno;
 
+static const int usage_err = 1;
+static const int open_failed = 2;
+static const int ftruncate_failed = 4;
+static const int truncate_failed = 4;
+
 off_t offset = 4095; // 4K
 
-off_t ext(const char *file_name, int kb_size)
+off_t ext(const char *file_name, int _4kb_size)
 {
     int mode = 0664; // umask 022, mode & ~umask = 0644
-    // 创建权限为mode&~umask的文件并以读写方式打开，返回文件描述符
-    int fd = open(file_name, O_CREAT | O_RDWR, mode);
-    if (-1 == fd) {
-        fprintf(stderr, "cause by open(): %s\n", strerror(errno));
-        assert(0);
-    }
+    int fd = open(file_name, O_CREAT | O_RDWR | O_EXCL, mode);
+    exit_if(-1 == fd, open_failed, stderr, "open %s failed.\n", file_name)
 
     off_t offt = 0;
-    for (int i = 0; i < kb_size; ++i) {
-        offt += lseek(fd, offset, SEEK_END);
+    for (int i = 0; i < _4kb_size; ++i) {
+        offt = lseek(fd, offset, SEEK_END) + 1l;
         write(fd, " ", 1);
     }
-
-    // use ftruncate and truncate
-    // int truncate(const char *path, off_t length);
-    // int ftruncate(int fd, off_t length);
 
     close(fd);
     return offt;
 }
 
+off_t ext_(const char *file_name, int _4kb_size)
+{
+    int mode = 0664;
+    int fd = open(file_name, O_CREAT | O_RDWR | O_EXCL, mode);
+    exit_if(-1 == fd, open_failed, stderr, "open %s failed.\n", file_name)
+
+    exit_if(-1 == ftruncate(fd, _4kb_size * offset), ftruncate_failed, stderr, "ext %s failed\n", file_name)
+    // exit_if(-1 == truncate(file_name, _4kb_size * offset), truncate_failed, stderr, "ext %s failed\n", file_name)
+
+    close(fd);
+    return _4kb_size * (offset + 1l);
+}
+
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
-        printf("usage: ./ext filename n-KB\n");
-        return -1;
-    }
+    exit_if(argc < 3, usage_err, stderr, "usage: %s <filename> <n-4KB>\n", argv[0])
+
     const char *file_name = argv[1];
-    int kb_size = atoi(argv[2]);
-    off_t offt = ext(file_name, kb_size);
-    printf("final write size = %zd\n", offt);
+    int _4kb_size = atoi(argv[2]);
+    exit_if(0 >= _4kb_size, usage_err, stderr, "<n-4KB> must be int\n")
+
+#ifdef EXT
+    off_t offt = ext(file_name, _4kb_size);
+    printf("final write size = %ld\n", offt);
+#elif defined(EXT_)
+    off_t offt = ext_(file_name, _4kb_size);
+    printf("final write size = %ld\n", offt);
+#endif
         
     return 0;
 }
